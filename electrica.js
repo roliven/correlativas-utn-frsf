f9e57969b4b1a0d17d539a61c9c18cecf3f886b6
roliven <rolivencia@live.com.ar>
2017-07-11 03:59:40 -0300

    Added Electrical Engineering career plan

---
+++ electrica.js
@@ -0,0 +1,345 @@
+cargarPlan(10, [
+    [
+        {
+            "ord": 1,
+            "nombre": "Análisis Matemático I",
+            "horas": 5,
+            "cursadasParaCursar": [],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": []
+        },
+        {
+            "ord": 2,
+            "nombre": "Álgebra",
+            "horas": 5,
+            "cursadasParaCursar": [],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": []
+        },
+        {
+            "ord": 3,
+            "nombre": "Ingeniería y Sociedad",
+            "horas": 2,
+            "cursadasParaCursar": [],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": []
+        },
+        {
+            "ord": 4,
+            "nombre": "Sistemas de Representación",
+            "horas": 3,
+            "cursadasParaCursar": [],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": []
+        },
+        {
+            "ord": 5,
+            "nombre": "Física I",
+            "horas": 5,
+            "cursadasParaCursar": [],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": []
+        },
+        {
+            "ord": 6,
+            "nombre": "Química General",
+            "horas": 5,
+            "cursadasParaCursar": [],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": []
+        },
+        {
+            "ord": 7,
+            "nombre": "Integración Eléctrica I",
+            "horas": 3,
+            "cursadasParaCursar": [],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": []
+        },
+        {
+            "ord": 8,
+            "nombre": "Fundamentos de Informática",
+            "horas": 2,
+            "cursadasParaCursar": [],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": []
+        }
+    ],
+    [
+        {
+            "ord": 9,
+            "nombre": "Física II",
+            "horas": 5,
+            "cursadasParaCursar": [1,5],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": [1,5]
+        },
+        {
+            "ord": 10,
+            "nombre": "Probabilidad y Estadística",
+            "horas": 3,
+            "cursadasParaCursar": [1,2],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": [1, 2]
+        },
+        {
+            "ord": 11,
+            "nombre": "Electrotecnia I",
+            "horas": 6,
+            "cursadasParaCursar": [
+                1,2,5
+            ],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": [
+               1,2,5
+            ]
+        },
+        {
+            "ord": 12,
+            "nombre": "Estabilidad",
+            "horas": 4,
+            "cursadasParaCursar": [2, 5],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": [2, 5]
+        },
+        {
+            "ord": 13,
+            "nombre": "Mecánica Técnica",
+            "horas": 2,
+            "cursadasParaCursar": [1, 5],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": [1, 5]
+        },
+        {
+            "ord": 14,
+            "nombre": "Integración Eléctrica II",
+            "horas": 3,
+            "cursadasParaCursar": [1,5,7],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": [1,5,7]
+        },
+        {
+            "ord": 15,
+            "nombre": "Inglés I",
+            "horas": 2,
+            "cursadasParaCursar": [],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": []
+        },
+        {
+            "ord": 16,
+            "nombre": "Análisis Matemático II",
+            "horas": 5,
+            "cursadasParaCursar": [1,2],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": [1,2]
+        },
+        {
+            "ord": 17,
+            "nombre": "Cálculo Numérico",
+            "horas": 2,
+            "cursadasParaCursar": [1, 2],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": [1, 2]
+        }
+    ],
+    [
+
+        {
+            "ord": 18,
+            "nombre": "Tecnología y Ensayo de Materiales Eléctricos",
+            "horas": 3,
+            "cursadasParaCursar": [6,9],
+            "aprobadasParaCursar": [1,5],
+            "aprobadasParaRendir": [6,9]
+        },
+        {
+            "ord": 19,
+            "nombre": "Instrumentos y Mediciones Eléctricas",
+            "horas": 6,
+            "cursadasParaCursar": [10,11,14],
+            "aprobadasParaCursar": [1,2,3,4,5,7],
+            "aprobadasParaRendir": [10,11,14]
+        },
+        {
+            "ord": 20,
+            "nombre": "Teoría de los Campos",
+            "horas": 3,
+            "cursadasParaCursar": [9,16],
+            "aprobadasParaCursar": [1,2,5],
+            "aprobadasParaRendir": [9,16]
+        },
+        {
+            "ord": 21,
+            "nombre": "Física III",
+            "horas": 2,
+            "cursadasParaCursar": [9,16],
+            "aprobadasParaCursar": [1,2,5],
+            "aprobadasParaRendir": [9,16]
+        },
+        {
+            "ord": 22,
+            "nombre": "Máquinas Eléctricas",
+            "horas": 6,
+            "cursadasParaCursar": [9,11,14],
+            "aprobadasParaCursar": [1,5,7,8],
+            "aprobadasParaRendir": [9,11,14]
+        },
+        {
+            "ord": 23,
+            "nombre": "Electrotecnia II",
+            "horas": 4,
+            "cursadasParaCursar": [9,11,16],
+            "aprobadasParaCursar": [1,2,5],
+            "aprobadasParaRendir": [9,11,16]
+        },
+        {
+            "ord": 24,
+            "nombre": "Termodinámica",
+            "horas": 3,
+            "cursadasParaCursar": [9,16],
+            "aprobadasParaCursar": [1,2,5],
+            "aprobadasParaRendir": [9,16]
+        },
+        {
+            "ord": 25,
+            "nombre": "Fundamentos para el Análisis de Señales",
+            "horas": 3,
+            "cursadasParaCursar": [16,17],
+            "aprobadasParaCursar": [1,2],
+            "aprobadasParaRendir": [16,17]
+        }
+    ],
+    [
+        {
+            "ord": 26,
+            "nombre": "Inglés II",
+            "horas": 2,
+            "cursadasParaCursar": [],
+            "aprobadasParaCursar": [15],
+            "aprobadasParaRendir": []
+        },
+        {
+            "ord": 27,
+            "nombre": "Economía",
+            "horas": 3,
+            "cursadasParaCursar": [14],
+            "aprobadasParaCursar": [3],
+            "aprobadasParaRendir": [14]
+        },
+        {
+            "ord": 28,
+            "nombre": "Electrónica I",
+            "horas": 8,
+            "cursadasParaCursar": [11],
+            "aprobadasParaCursar": [1,5],
+            "aprobadasParaRendir": [11]
+        },
+        {
+            "ord": 29,
+            "nombre": "Máquinas Eléctricas II",
+            "horas": 6,
+            "cursadasParaCursar": [18,19,20,22,23],
+            "aprobadasParaCursar": [6,9,10,11,14,16],
+            "aprobadasParaRendir": [18,19,20,22,23]
+        },
+        {
+            "ord": 30,
+            "nombre": "Seguridad, Riesgo Eléctrico y Medio Ambiente",
+            "horas": 2,
+            "cursadasParaCursar": [11,20],
+            "aprobadasParaCursar": [1,2,5,9,16],
+            "aprobadasParaRendir": [11,20]
+        },
+        {
+            "ord": 31,
+            "nombre": "Instalaciones Eléctricas y Luminotecnia",
+            "horas": 6,
+            "cursadasParaCursar": [18,22,23],
+            "aprobadasParaCursar": [6,9,11,14,15,16],
+            "aprobadasParaRendir": [18,22,23]
+        },
+        {
+            "ord": 32,
+            "nombre": "Control Automático",
+            "horas": 5,
+            "cursadasParaCursar": [23,25],
+            "aprobadasParaCursar": [11,16],
+            "aprobadasParaRendir": [23,25]
+        },
+        {
+            "ord": 33,
+            "nombre": "Máquinas Térmicas, Hidráulicas y de Fluidos",
+            "horas": 3,
+            "cursadasParaCursar": [12,13,14],
+            "aprobadasParaCursar": [9,16],
+            "aprobadasParaRendir": [12,13,14]
+        },
+        {
+            "ord": 34,
+            "nombre": "Legislación",
+            "horas": 2,
+            "cursadasParaCursar": [14],
+            "aprobadasParaCursar": [3],
+            "aprobadasParaRendir": [14]
+        }
+    ],
+    [
+        {
+            "ord": 35,
+            "nombre": "Electrónica II",
+            "horas": 3,
+            "cursadasParaCursar": [28],
+            "aprobadasParaCursar": [11],
+            "aprobadasParaRendir": [28]
+        },
+        {
+            "ord": 36,
+            "nombre": "Generación, Transmisión y Distribución de Energía Eléctrica",
+            "horas": 6,
+            "cursadasParaCursar": [21,29,33],
+            "aprobadasParaCursar": [11,13,18,22,23,24],
+            "aprobadasParaRendir": [21,29,33]
+        },
+        {
+            "ord": 37,
+            "nombre": "Sistemas de Potencia",
+            "horas": 4,
+            "cursadasParaCursar": [29],
+            "aprobadasParaCursar": [18,22,23],
+            "aprobadasParaRendir": [29]
+        },
+        {
+            "ord": 38,
+            "nombre": "Accionamientos y Controles Eléctricos",
+            "horas": 4,
+            "cursadasParaCursar": [28,29,32],
+            "aprobadasParaCursar": [11,18,22,23,25],
+            "aprobadasParaRendir": [28,29,32]
+        },
+        {
+            "ord": 39,
+            "nombre": "Organización y Administración de Empresas",
+            "horas": 2,
+            "cursadasParaCursar": [27,34],
+            "aprobadasParaCursar": [14],
+            "aprobadasParaRendir": [27,34]
+        },
+        {
+            "ord": 40,
+            "nombre": "Proyecto Final",
+            "horas": 3,
+            "cursadasParaCursar": [29,31,32],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,16,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39]
+        },
+        {
+            "ord": 41,
+            "nombre": "Práctica Supervisada",
+            "horas": 0,
+            "cursadasParaCursar": [],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": []
+        }
+    ]
+]);
