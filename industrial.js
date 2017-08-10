27d88c82e3174ac809f40b2a62d5b57a06f1e524
roliven <rolivencia@live.com.ar>
2017-08-10 00:05:11 -0300

    Added Industrial Engineering career

---
+++ industrial.js
@@ -0,0 +1,357 @@
+cargarPlan(10, [
+    [
+        {
+            "ord": 1,
+            "nombre": "Análisis Matemático I",
+            "horas": 5,
+            "cursadasParaCursar": [],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": []
+        },
+        {
+            "ord": 2,
+            "nombre": "Química General",
+            "horas": 5,
+            "cursadasParaCursar": [],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": []
+        },
+        {
+            "ord": 3,
+            "nombre": "Sistemas de Representación",
+            "horas": 3,
+            "cursadasParaCursar": [],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": []
+        },
+        {
+            "ord": 4,
+            "nombre": "Informática I",
+            "horas": 3,
+            "cursadasParaCursar": [],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": []
+        },
+        {
+            "ord": 5,
+            "nombre": "Pensamiento Sistémico",
+            "horas": 3,
+            "cursadasParaCursar": [],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": []
+        },
+        {
+            "ord": 6,
+            "nombre": "Física I",
+            "horas": 5,
+            "cursadasParaCursar": [],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": []
+        },
+        {
+            "ord": 7,
+            "nombre": "Álgebra y Geometría Analítica",
+            "horas": 5,
+            "cursadasParaCursar": [],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": []
+        },
+        {
+            "ord": 8,
+            "nombre": "Ingeniería y Sociedad",
+            "horas": 2,
+            "cursadasParaCursar": [],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": []
+        }
+    ],
+    [
+        {
+            "ord": 9,
+            "nombre": "Análisis Matemático II",
+            "horas": 5,
+            "cursadasParaCursar": [1,7],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": [1,7]
+        },
+        {
+            "ord": 10,
+            "nombre": "Administración General",
+            "horas": 4,
+            "cursadasParaCursar": [4,5,7,8],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": [4,5,7,8]
+        },
+        {
+            "ord": 11,
+            "nombre": "Probabilidad y Estadística",
+            "horas": 3,
+            "cursadasParaCursar": [1,7],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": [1,7]
+        },
+        {
+            "ord": 12,
+            "nombre": "Ciencia de los Materiales",
+            "horas": 4,
+            "cursadasParaCursar": [2,6],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": [2,6]
+        },
+        {
+            "ord": 13,
+            "nombre": "Física II",
+            "horas": 5,
+            "cursadasParaCursar": [1,6],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": [1,6]
+        },
+        {
+            "ord": 14,
+            "nombre": "Economía General",
+            "horas": 4,
+            "cursadasParaCursar": [1,5,8],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": [1,5,8]
+        },
+        {
+            "ord": 15,
+            "nombre": "Informática II",
+            "horas": 3,
+            "cursadasParaCursar": [4],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": [4]
+        },
+        {
+            "ord": 16,
+            "nombre": "Inglés I",
+            "horas": 2,
+            "cursadasParaCursar": [],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": []
+        }
+    ],
+    [
+        {
+            "ord": 17,
+            "nombre": "Costos y Presupuestos",
+            "horas": 3,
+            "cursadasParaCursar": [10,14],
+            "aprobadasParaCursar": [1,4,5,7,8],
+            "aprobadasParaRendir": [10,14]
+        },
+        {
+            "ord": 18,
+            "nombre": "Estudio del Trabajo",
+            "horas": 4,
+            "cursadasParaCursar": [10,14],
+            "aprobadasParaCursar": [1,4,5,7,8],
+            "aprobadasParaRendir": [10,14]
+        },
+        {
+            "ord": 19,
+            "nombre": "Comercialización",
+            "horas": 3,
+            "cursadasParaCursar": [10,11,14],
+            "aprobadasParaCursar": [1,4,5,7,8],
+            "aprobadasParaRendir": [10,11,14]
+        },
+        {
+            "ord": 20,
+            "nombre": "Termodinámica y Máquinas Térmicas",
+            "horas": 4,
+            "cursadasParaCursar": [2,13],
+            "aprobadasParaCursar": [1,6],
+            "aprobadasParaRendir": [2,13]
+        },
+        {
+            "ord": 21,
+            "nombre": "Estática y Resistencia de Materiales",
+            "horas": 4,
+            "cursadasParaCursar": [9,12],
+            "aprobadasParaCursar": [1,2,6,7],
+            "aprobadasParaRendir": [9,12]
+        },
+        {
+            "ord": 22,
+            "nombre": "Mecánica de los Fluidos",
+            "horas": 3,
+            "cursadasParaCursar": [9],
+            "aprobadasParaCursar": [1,6,7],
+            "aprobadasParaRendir": [9]
+        },
+        {
+            "ord": 23,
+            "nombre": "Economía de la Empresa",
+            "horas": 3,
+            "cursadasParaCursar": [10,14],
+            "aprobadasParaCursar": [1,4,5,7,8],
+            "aprobadasParaRendir": [10,14]
+        },
+        {
+            "ord": 24,
+            "nombre": "Electrotecnia y Máquinas Eléctricas",
+            "horas": 5,
+            "cursadasParaCursar": [13],
+            "aprobadasParaCursar": [1,6],
+            "aprobadasParaRendir": [9,13]
+        },
+
+        {
+            "ord": 25,
+            "nombre": "Análisis Numérico y Cálculo Avanzado",
+            "horas": 2,
+            "cursadasParaCursar": [9],
+            "aprobadasParaCursar": [1,7],
+            "aprobadasParaRendir": [9]
+        }
+    ],
+    [
+        {
+            "ord": 26,
+            "nombre": "Seguridad, Higiene e Ingeniería Ambiental",
+            "horas": 3,
+            "cursadasParaCursar": [18],
+            "aprobadasParaCursar": [10,11],
+            "aprobadasParaRendir": [18]
+        },
+        {
+            "ord": 27,
+            "nombre": "Investigación Operativa",
+            "horas": 4,
+            "cursadasParaCursar": [9,11],
+            "aprobadasParaCursar": [1,7],
+            "aprobadasParaRendir": [9,11]
+        },
+        {
+            "ord": 28,
+            "nombre": "Procesos Industriales",
+            "horas": 5,
+            "cursadasParaCursar": [18,20,24],
+            "aprobadasParaCursar": [2,10,11,13],
+            "aprobadasParaRendir": [18,20,24]
+        },
+        {
+            "ord": 29,
+            "nombre": "Mecánica y Mecanismos",
+            "horas": 3,
+            "cursadasParaCursar": [9],
+            "aprobadasParaCursar": [1,6,7],
+            "aprobadasParaRendir": [9]
+        },
+        {
+            "ord": 30,
+            "nombre": "Evaluación de Proyectos",
+            "horas": 5,
+            "cursadasParaCursar": [18,19,23],
+            "aprobadasParaCursar": [10,11,14,16],
+            "aprobadasParaRendir": [17,18,19,23]
+        },
+        {
+            "ord": 31,
+            "nombre": "Planificación y Control de la Producción",
+            "horas": 4,
+            "cursadasParaCursar": [18],
+            "aprobadasParaCursar": [10,11],
+            "aprobadasParaRendir": [18]
+        },
+        {
+            "ord": 32,
+            "nombre": "Diseño de Producto",
+            "horas": 2,
+            "cursadasParaCursar": [15,19],
+            "aprobadasParaCursar": [3,4,10,11,14],
+            "aprobadasParaRendir": [15,19]
+        },
+        {
+            "ord": 33,
+            "nombre": "Inglés II",
+            "horas": 2,
+            "cursadasParaCursar": [16],
+            "aprobadasParaCursar": [],
+            "aprobadasParaRendir": [16]
+        },
+        {
+            "ord": 34,
+            "nombre": "Instalaciones Industriales",
+            "horas": 3,
+            "cursadasParaCursar": [20,21,22,24],
+            "aprobadasParaCursar": [2,9,12,13],
+            "aprobadasParaRendir": [20,21,22,24]
+        },
+        {
+            "ord": 35,
+            "nombre": "Legislación",
+            "horas": 2,
+            "cursadasParaCursar": [],
+            "aprobadasParaCursar": [10],
+            "aprobadasParaRendir": []
+        }
+    ],
+    [
+        {
+            "ord": 36,
+            "nombre": "Mantenimiento",
+            "horas": 3,
+            "cursadasParaCursar": [34],
+            "aprobadasParaCursar": [20,21,24],
+            "aprobadasParaRendir": [34]
+        },
+        {
+            "ord": 37,
+            "nombre": "Manejo de Materiales y Distribución de Plantas",
+            "horas": 3,
+            "cursadasParaCursar": [18,29],
+            "aprobadasParaCursar": [9,10,11],
+            "aprobadasParaRendir": [18,29]
+        },
+        {
+            "ord": 38,
+            "nombre": "Comercio Exterior",
+            "horas": 3,
+            "cursadasParaCursar": [30],
+            "aprobadasParaCursar": [18,19,23],
+            "aprobadasParaRendir": [30]
+        },
+        {
+            "ord": 39,
+            "nombre": "Relaciones Industriales",
+            "horas": 3,
+            "cursadasParaCursar": [18],
+            "aprobadasParaCursar": [10,11],
+            "aprobadasParaRendir": [18]
+        },
+        {
+            "ord": 40,
+            "nombre": "Proyecto Final",
+            "horas": 3,
+            "cursadasParaCursar": [25,26,27,28,30,31],
+            "aprobadasParaCursar": [18,19,20,21,22,23,24,33],
+            "aprobadasParaRendir": [36,37,38,39,41,42,43]
+        },
+        {
+            "ord": 41,
+            "nombre": "Ingeniería en Calidad",
+            "horas": 3,
+            "cursadasParaCursar": [18],
+            "aprobadasParaCursar": [10,11],
+            "aprobadasParaRendir": [18]
+        },
+        {
+            "ord": 42,
+            "nombre": "Control de Gestión",
+            "horas": 3,
+            "cursadasParaCursar": [17,23],
+            "aprobadasParaCursar": [10,14],
+            "aprobadasParaRendir": [17,23]
+        },
+        {
+            "ord": 43,
+            "nombre": "Práctica Supervisada",
+            "horas": 3,
+            "cursadasParaCursar": [26,27,28,29,30,31,32,33,34,35],
+            "aprobadasParaCursar": [26,27,28,29,30,31,32,33,34,35],
+            "aprobadasParaRendir": [26,27,28,29,30,31,32,33,34,35]
+        }
+    ]
+]);
